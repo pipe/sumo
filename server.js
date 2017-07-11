fc29d1ccf9c63d3bdeb4d32b52589355b09cdf6b
Birgit Pohl <BirgitPohl@users.noreply.github.com>
2017-07-11 16:51:03 +0200

    Moved Gyronorm to the client side

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 let app, express, path, port, router;
 
-let GyroNorm = require('gyronorm').GyroNorm;
 express = require('express');
 path = require('path');
 port = process.env.PORT || 8080;
 app = express();
 
 app.use(express["static"](path.join(__dirname, '/controller')));
-app.use(express["static"](path.join(__dirname, '/node_modules/gyronorm')));
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + "/controller/control.html"));
